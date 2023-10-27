ea1dd9666e80de0f6e60c326efee1085e43282d0
Moses-Onche <onchemoses38@gmail.com>
2023-10-27 09:36:38 +0100

    Try heading tags in a component

---
+++ mini/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Heading />
+      </header>
+    </div>
+  );
+}
+
+function Heading() {
+  return (
+    <div>
+      <h1>Heading type 1</h1>
+      <h2>Heading type 2</h2>
+      <h3>Heading type 3</h3>
+      <h4>Heading type 4</h4>
+      <h5>Heading type 5</h5>
+      <h6>Heading type 6</h6>
+      <h6>Comparing a heading type 6...</h6>
+      <p>to a paragraph</p>
+    </div>
+  );
+}
+
+export default App;
